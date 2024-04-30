4267e6a6a29695be84d3bef0dc5a26297c5692c6
WenxuanChen1 <wenxuan146@gmail.com>
2024-04-30 18:03:31 -0400

    final project

---
+++ Final project/allGame.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    const settings = {
+        async: true,
+        crossDomain: true,
+        url: 'https://gamerpower.p.rapidapi.com/api/filter?platform=epic-games-store.steam.android&type=game.loot',
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'd2129940b5msh616ad49c215b1f7p10812bjsnb839e4e08eef',
+            'X-RapidAPI-Host': 'gamerpower.p.rapidapi.com'
+        }
+    };
+
+    
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        var table = '<table><thead><tr><th>Image</th><th>Title</th><th>Users</th><th>Worth</th><th>Platform</th><th>Type</th></tr></thead><tbody>';
+        for (var i = 0; i < response.length; i++) {
+            table += '<tr>';
+            table += '<td><img src="' + response[i].image + '" alt="' + response[i].title + '"></td>';
+            table += '<td>' + response[i].title + '</td>';
+            table += '<td>' + response[i].users + '</td>';
+            table += '<td>' + response[i].worth + '</td>';
+            table += '<td>' + response[i].platforms + '</td>';
+            table += '<td>' + response[i].type + '</td>';
+            table += '</tr>';
+        }
+        
+        table += '</tbody></table>';
+        $('#tableContainer').append(table);
+    });
+});
+
+
+
+
+
+
